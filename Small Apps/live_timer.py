45466ec5c0dd839077dab6f0823433c883cab22b
Gerile3 <beko.girgin@gmail.com>
2020-05-02 13:53:36 +0300

    Update general folder, initial push for steamprice,livetimer

---
+++ Small Apps/live_timer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
+
+def alive(date):
+    date = date.split("/")
+
+    if len(date) == 3:
+        today = datetime.now()
+        birthday = datetime(year=int(date[2]), month=int(date[1]), day=int(date[0]))
+
+        year = today.year - birthday.year
+        month = today.month - birthday.month
+        day = today.day - birthday.day
+
+        if month < 0:
+            year = year - 1
+            month = abs(today.month - birthday.month)
+
+        if day < 0:
+            month = month - 1
+            day = abs(today.day - birthday.day)
+
+        months = year * 12 + month
+        days = (today - birthday).days
+        hours = days * 24 + today.hour
+        mins = hours * 60 + today.minute
+        seconds = mins * 60 + today.second
+
+        alive = "You are alive for {} years, {} months, {} days!".format(year, month, day)
+        alive2 = "In detail thats like:\n {} months\n {} days\n {} hours\n {} mins\n {} seconds".format(months, days, hours,
+                                                                                                        mins, seconds)
+
+        print(alive, alive2)
+
+
+if __name__ == "__main__":
+    date = input("Enter your birthday (day/month/year): ")
+    alive(date)
---
+++ Small Apps/steamprice.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def check_price(word):
+    """Checks price and bunch of other infos for a given game name"""
+    info = {}
+    url = "https://www.steamprices.com/us/search/?q=" + word + "&p=1"
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+                                            'Accept': 'text/css,*/*;q=0.1',
+                                            'Accept-Language': 'en-GB,en;q=0.5',
+                                            'Accept-Encoding': 'gzip, deflate, br',
+                                            'DNT': '1',
+                                            'Connection': 'keep-alive'})
+    soup = BeautifulSoup(response.content, "lxml")
+
+    if response.status_code == 200:
+        result = soup.find("div", class_="details")
+        summary = soup.find("div", class_="summary")
+
+        pic = result.find('img', class_="lazy")['data-original']
+        title = result.find('h4', class_="title").text
+        try:
+            price = result.find("td", class_="price_curent").text
+        except AttributeError:
+            price = "Free"
+        try:
+            meta_note = summary.find("a")["data-original-title"]
+            user_note = summary.find_all("a")[1]["data-original-title"]
+        except TypeError:
+            meta_note = "None"
+            user_note = "None"
+
+        gtype = summary.find("div", class_="pull-right").text
+        date = summary.find("div", class_="pull-right").span["data-original-title"]
+        date = re.sub(r'<em>|</em>|<br />|<sup>|nd|</sup>', '', date)
+
+        info.update({'Picture': pic, 'Title': title, 'Price': price, 'UserNote': user_note, 'MetaNote': meta_note, 'GameType': gtype, 'Date': date})
+
+        return info
+
+
+if __name__ == "__main__":
+    game = check_price("dishonored")
+    print(game)
