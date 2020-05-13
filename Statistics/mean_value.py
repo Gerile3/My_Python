3767c6804c50757fe558c17c32f8876b6f58c3ad
Gerile3 <beko.girgin@gmail.com>
2020-05-13 14:56:20 +0300

    initial push for statistics

---
+++ Statistics/mean_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Calculate Mean for the given list"""
+
+
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+
+
+if __name__ == "__main__":
+    steps = [4000, 3200, 3860, 4090, 3710, 2069, 987]
+    mean = mean(steps)
+
+    print("Your average step/day is:", round(mean))
