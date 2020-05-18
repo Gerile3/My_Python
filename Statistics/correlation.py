ad90553b5483ebb6e93ced615612e6004f246037
Gerile3 <beko.girgin@gmail.com>
2020-05-18 13:34:00 +0300

    fix typo

--- Statistics/correlation.py
+++ Statistics/correlation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
 import matplotlib.pyplot as plt
 
 
 def correlation(list1, list2):
+    """Calculates correlation for the given lists"""
     n = len(list1)
     sum_xy = sum([x * y for x, y in zip(list1, list2)])
     sum_x = sum(list1)
@@ -20,12 +20,14 @@ if __name__ == "__main__":
     list1 = [83, 85, 84, 96, 94, 86, 87, 97, 97, 85]
     list2 = [85, 87, 86, 97, 96, 88, 89, 98, 98, 87]
 
-    correlation = correlation(list1, list2)
+    if len(list1) == len(list2):
+        correlation = correlation(list1, list2)
+        print("Correlation coefficient of the two lists: {:.2f}".format(correlation))
 
-    print("Correlation coefficient of the two lists: {:.2f}".format(correlation))
-
-    # if plot closer to be diagonal that means correlation is high,
-    # strong relationship between these data set
-    plt.scatter(list1, list2)
-    plt.grid()
-    plt.show()
+        # if plot closer to be diagonal that means correlation is high,
+        # strong relationship between these data set
+        plt.scatter(list1, list2)
+        plt.grid()
+        plt.show()
+    else:
+        print("Provide lists that has same lenght")
