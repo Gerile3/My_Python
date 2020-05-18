d098bec8ded0bf72d9babae4a17d6b1728571bad
Gerile3 <beko.girgin@gmail.com>
2020-05-18 13:34:37 +0300

    initial push for variance & std

---
+++ Statistics/find_variance.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from mean_value import mean
+
+
+def variance(numbers):
+    result = []
+    mean_val = mean(numbers)
+    print("Mean of notes: {:.2f}".format(mean_val))
+
+    for number in numbers:
+        result.append((number - mean_val)**2)
+
+    var = sum(result) / len(numbers)
+
+    return var
+
+
+def std_deviation(var):
+    return var**0.5
+
+
+if __name__ == "__main__":
+    notes = [90, 56, 45, 60, 72, 75, 38, 40, 52]
+
+    variance = variance(notes)
+    std = std_deviation(variance)
+
+    print("Variance of notes: {:.2f}".format(variance))
+    print("Standart deviation of notes {:.2f}".format(std))
