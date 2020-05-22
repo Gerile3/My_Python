bdf7f26e3a6f19cc2b4e4a61b539cb38789ef04e
Gerile3 <beko.girgin@gmail.com>
2020-05-22 14:45:47 +0300

    add create_teacher method

--- Sql/School_app/teacher.py
+++ Sql/School_app/teacher.py
@@ -6,3 +6,15 @@ class Teacher:
         self.Surname = Surname
         self.Birthdate = Birthdate
         self.Gender = Gender
+
+    @staticmethod
+    def create_teacher(obj):
+        listo = []
+
+        if isinstance(obj, tuple):
+            listo.append(Teacher(obj[0], obj[1], obj[2], obj[3], obj[4], obj[5]))
+        else:
+            for i in obj:
+                listo.append(Teacher(i[0], i[1], i[2], i[3], i[4], i[5]))
+
+        return listo
