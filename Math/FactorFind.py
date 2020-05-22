8b5aa4d2dd6d80ba0d15fe45237421b267152716
Gerile3 <beko.girgin@gmail.com>
2020-05-22 13:47:01 +0300

    add ability to factor expressions

--- Math/FactorFind.py
+++ Math/FactorFind.py
@@ -1,11 +1,30 @@
+from sympy import factor, sympify, SympifyError
+
+
 def factors(num):
-    '''Find the factors of an integer v.0.1'''
+    '''Find the factors of an integer'''
     return [i for i in range(1, num + 1) if num % i == 0]
 
 
+def factors_exp(expr):
+    '''Find the factors of an expression'''
+    return factor(expr)
+
+
 if __name__ == '__main__':
-    num = int(input('Your Number Please: '))
-    if num > 0:
-        print("Factors of {}: {}".format(num, factors(num)))
-    else:
-        print('Please enter a positive integer')
+    choice = input("1-) Number\n2-) Expression\n>>")
+
+    if choice == "1":
+        num = int(input('Enter Number to find factor: '))
+        if num > 0:
+            print("Factors of {}: {}".format(num, factors(num)))
+        else:
+            print('Please enter a positive integer')
+
+    elif choice == "2":
+        try:
+            expr = sympify(input('Enter an expression to factorize: '))
+        except SympifyError:
+            print('Invalid expression entered as input')
+        else:
+            print("Factors of ({}): {}".format(expr, factors_exp(expr)))
