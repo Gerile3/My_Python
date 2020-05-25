187df1cd50d49490a022692658507cc1b7b7f6d4
Gerile3 <beko.girgin@gmail.com>
2020-05-25 13:12:16 +0300

    improved summation and add ability to accept user provided terms

--- Math/series.py
+++ Math/series.py
@@ -1,18 +1,31 @@
-from sympy import Symbol, pprint
+from sympy import Symbol, pprint, summation
 
 
-def print_series(n, x_value):
-    x = Symbol('x')
-    series = x
-    for i in range(2, n + 1):
-        series = series + (x**i) / i
-    pprint(series)
+def add_series(expr, num, values):
+    """Print addition of series given by user, if values of the terms provided show the result as well"""
+    n = Symbol("n")
+    series = summation(expr, (n, 1, num))
 
-    series_value = series.subs({x: x_value})
-    print('Value of the series at {}: {:.2f}'.format(x_value, series_value))
+    if values:
+        listo = []
+
+        values = values.split(",")
+        for i in values:
+            listo.append(tuple(i.split("=")))
+
+        listo = dict(listo)
+        series_value = series.subs(listo)
+        print("Addition of the series: {}, result: {}".format(series, series_value))
+
+    else:
+        pprint(series)
 
 
 if __name__ == '__main__':
-    n = int(input('Enter the number of terms you want in the series: '))
-    x_value = float(input('Enter the value of x at which you want to evaluate the series: '))
-    print_series(n, x_value)
+    # expr = a+(n-1)*d
+    # num = 3
+    # values = a=5,d=4
+    expr = input('Enter the nth term of series: ')
+    num = int(input('Enter the number of terms you want in the series: '))
+    values = input("Enter the variable values(a=3,b=4... etc, leave blank if not desired):")
+    add_series(expr, num, values)
