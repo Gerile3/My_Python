2d4b58ce6a224968418df1eadee59b3a86f84caa
Gerile3 <beko.girgin@gmail.com>
2020-06-08 12:59:54 +0300

    add new conversions

--- Small Apps/Unit_converter.py
+++ Small Apps/Unit_converter.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 '''
-Unit converter: Miles and Kilometers
+Unit converter: Miles and Kilometers and bunch of others
 '''
 
 
 def print_menu():
-    print('1. Kilometers to Miles')
-    print('2. Miles to Kilometers')
+    print("1. Kilometers to Miles\n2. Miles to Kilometers\n3. Feet to Inch and Cm\n4. Feet to miles,yard,inch",
+          "\n5. Celcius to Kelvin and Fahrenheit\n6. Pascal to Mmhg and Atm and Kpa")
 
 
 def km_miles():
@@ -20,13 +21,58 @@ def miles_km():
     print('Distance in kilometers: {0}'.format(km))
 
 
+def inchtocm():
+    # feet = 12 inch
+    # 1 inch = 2.54 cm
+
+    result = float(input("Enter Feet: "))
+
+    print("{} feet equals to {} inch and {} cm".format(result, result * 12, result * 12 * 2.54))
+
+
+def feettoinch():
+    # feet = 0.333 yard
+    # feet = 0.0001893939 miles
+    # feet = 12 inch
+
+    result = float(input("Enter in feet: "))
+    print("{} feets equals to {} yard, {} miles, {} inches".format(result, result * 0.333, result * 0.0001893939, result * 12))
+
+
+def ctofk():
+    t = float(input("Enter temperature: "))
+    f = (t / 5) * 9 + 32
+    k = t + 273.15
+
+    print("{} *C equals {} F and {} Kelvin".format(t, f, k))
+
+
+def pascalto():
+    # 1 kPa = 0.145 psi
+    kpa = float(input("Enter kilopascal: "))
+    psi = kpa * 0.14504
+    mmHg = kpa * 7.50062
+    atm = kpa * 0.00987
+
+    print("{} kpa equals {:.2f} psi, {:.2f} mmHg, {:.2f} atm".format(kpa, psi, mmHg, atm))
+
+
 if __name__ == '__main__':
     print_menu()
-    choice = input('Which conversion would you like to do?: ')
+    choice = input('Which conversion would you like to do? (q to quit): ')
     if choice == '1':
         km_miles()
-    if choice == '2':
+    elif choice == '2':
         miles_km()
-
-# todo:
-# temp, mass
+    elif choice == '3':
+        inchtocm()
+    elif choice == '4':
+        feettoinch()
+    elif choice == '5':
+        ctofk()
+    elif choice == '6':
+        pascalto()
+    elif choice.lower() == "q":
+        exit(1)
+    else:
+        print("Wrong input")
